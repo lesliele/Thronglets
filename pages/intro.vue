def19306a11436649195f41805a83e988511ae52
leslie_kai <1224622183@qq.com>
2025-04-12 00:28:00 +0800

    Update intro.vue

--- pages/intro.vue
+++ pages/intro.vue
@@ -45,4 +45,34 @@
       <p class="mt-6 text-xl leading-8">5.Select "Get Game" to proceed with the download.</p>
     </div>
   </div>
+  <div class="bg-gray-900 pt-10">
+    <div class="mx-auto max-w-7xl text-base leading-7 text-slate-300 px-4 sm:px-6 lg:px-8">
+      <h2 class="font-semibold text-2xl text-white">How Netflix subscribers can download Thronglets game?</h2>
+      <p class="mt-6 text-xl leading-8">Accessing Netflix Games on iOS and Android involves slightly different steps, but both require a Netflix subscription which currently costs $7 per month.</p>
+      <h3 class="font-semibold text-xl text-white">For iOS users who subscribe:</h3>
+      <p class="mt-6 text-xl leading-8">1.First, download <a class="font-semibold text-orange-500 hover:text-orange-400" href="https://apps.apple.com/us/app/netflix/id363590051" target="_blank">the Netflix application</a> on your iPhone or iPad.</p>
+      <p class="mt-6 text-xl leading-8">2.Launch the Netflix app.</p>
+      <p class="mt-6 text-xl leading-8">3.Select your profile and log in to your account.</p>
+      <p class="mt-6 text-xl leading-8">4.Find the "Mobile Games" section</p>
+      <p class="mt-6 text-xl leading-8">5.Find the Thronglets game, tap "Get Game".</p>
+      <h3 class="font-semibold text-xl text-white">For Android users who subscribe:</h3>
+      <p class="mt-6 text-xl leading-8">1.Begin by downloading <a class="font-semibold text-orange-500 hover:text-orange-400" href="https://play.google.com/store/apps/details?id=com.netflix.mediaclient&hl=en_US" target="_blank">the Netflix app</a> onto your Android device.</p>
+      <p class="mt-6 text-xl leading-8">2.Open the Netflix application.</p>
+      <p class="mt-6 text-xl leading-8">3.Choose your profile and sign in.</p>
+      <p class="mt-6 text-xl leading-8">4.Locate and tap "Games" at the bottom of the screen.</p>
+      <p class="mt-6 text-xl leading-8">5.Select "Get Game" to proceed with the download.</p>
+    </div>
+  </div>
+  <div class="bg-gray-900 pt-10">
+    <div class="mx-auto max-w-7xl text-base leading-7 text-slate-300 px-4 sm:px-6 lg:px-8">
+      <h2 class="font-semibold text-2xl text-white">Human type in Thronglets game</h2>
+      <p class="mt-6 text-xl leading-8 text-orange-500 mb-4">What is your human type on Thronglets? Welcome to join the Reddit community for discussion.</p>
+      <div class="grid grid-cols-4 gap-5">
+        <blockquote class="twitter-tweet"><p lang="und" dir="ltr"><a href="https://twitter.com/hashtag/Thronglets?src=hash&amp;ref_src=twsrc%5Etfw">#Thronglets</a> lol <a href="https://t.co/FMev6KM1kY">pic.twitter.com/FMev6KM1kY</a></p>&mdash; him (@gmfker) <a href="https://twitter.com/gmfker/status/1910337020859461676?ref_src=twsrc%5Etfw">April 10, 2025</a></blockquote>
+        <blockquote class="twitter-tweet"><p lang="en" dir="ltr">Apparently, you get a &quot;type&quot; when you finish the Thronglets game, depending on how you have played it.<br><br>I can see people posting their type over the next days. <br><br>The game is actually nice. Undertale vibes 👀<br><br>Black Mirror delivered<br><br>AeAxtBjuSRbhUJLE7LSgbbW2ycHG4cBMoQw75AsTpump <a href="https://t.co/YbUOuXk0Yw">pic.twitter.com/YbUOuXk0Yw</a></p>&mdash; Crackodile (@NotCrackodile) <a href="https://twitter.com/NotCrackodile/status/1910448012209315997?ref_src=twsrc%5Etfw">April 10, 2025</a></blockquote>
+        <blockquote class="twitter-tweet"><p lang="en" dir="ltr">That&#39;s crazy. I was so nice<br>🥚 🍎 ❤️ <a href="https://twitter.com/hashtag/thronglets?src=hash&amp;ref_src=twsrc%5Etfw">#thronglets</a> <a href="https://t.co/H2IKZdjmJB">pic.twitter.com/H2IKZdjmJB</a></p>&mdash; 😈 (@DabiDoobie) <a href="https://twitter.com/DabiDoobie/status/1910515267702075853?ref_src=twsrc%5Etfw">April 11, 2025</a></blockquote>
+        <blockquote class="twitter-tweet"><p lang="qme" dir="ltr">🥚 🍎 ❤️ <a href="https://twitter.com/hashtag/thronglets?src=hash&amp;ref_src=twsrc%5Etfw">#thronglets</a> <a href="https://t.co/lDDBQ46h3Q">pic.twitter.com/lDDBQ46h3Q</a></p>&mdash; Hector Manuel (@27hectormanuel) <a href="https://twitter.com/27hectormanuel/status/1910287413659709635?ref_src=twsrc%5Etfw">April 10, 2025</a></blockquote>
+      </div>
+    </div>
+  </div>
 </template>
